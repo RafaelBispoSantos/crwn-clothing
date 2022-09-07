0cd74bbc399cf9b1f47ab597497c048395289fae
Rafael Bispo <rafaelbispodev@outlook.com>
2022-09-07 14:05:54 -0300

    created link home page

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,12 @@
 import "./category-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div className="container">
+    <div className="container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{
